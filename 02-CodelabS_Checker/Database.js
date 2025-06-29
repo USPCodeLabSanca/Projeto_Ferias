69e13c486ae23345e30579b88e88494c6916a13b
Arthur Santana <90651161+arthursantana-dev@users.noreply.github.com>
2025-06-29 18:29:26 -0300

    commit inicial

---
+++ 02-CodelabS_Checker/Database.js
@@ -0,0 +1,19 @@
+import { Sequelize } from "sequelize"
+
+import 'dotenv/config'
+
+const sequelize = new Sequelize(
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        port: process.env.DB_PORT
+    }
+)
+
+sequelize.authenticate().then(() => {
+    console.log("conectou");
+    
+})
