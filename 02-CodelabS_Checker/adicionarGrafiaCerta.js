7a9ad8fbfac6d5191b070eb92c3edde11da975f2
CLQueiroz <ceciliaqueiroz@usp.br>
2025-07-08 00:34:30 -0300

    terminando o verificador de palavras, falta só trocar GET pra POST

---
+++ 02-CodelabS_Checker/adicionarGrafiaCerta.js
@@ -0,0 +1,23 @@
+
+async function addCorreto(palavra){
+const { leitorDeJSON } = require('./verificaErros.js');
+const { editorJSON } = require('./verificaErros.js');
+
+const corretos = await leitorDeJson("validos.json");
+
+if (!corretos)
+    console.error('o "validos.json" não abriu de jeito nenhum');
+    return;
+
+else {
+    const jaTaNoArq = corretos.some(coisa => palavra === coisa);
+    
+    if (!jaTaNoArq){
+        corretos.push(palavra);
+        await editorJSON(corretos);}
+
+    return;
+}
+
+}
+
--- 02-CodelabS_Checker/verificaErros.js
+++ 02-CodelabS_Checker/verificaErros.js
@@ -8,9 +8,12 @@
         }
 
         catch(error) {
-            console.error(`Deu ruim pra ler ${nomeArq}`, error);
+            console.error(`Deu ruim pra ler "${nomeArq}"`, error);
+            return [];
         }
     };
+
+    module.exports = { leitorDeJSON }; 
 
     async function editorJSON(nomeArq, vaiEscreverOque){
         try{
@@ -21,22 +24,42 @@
             console.error('Deu ruim pra editar:', error);
         }
     }
+
+    module.exports = { editorJSON }; 
+
+    function estatisticas(aray){
+        //isso aqui atualiza a estatistica de erro de cada objeto no aray
+            let contagem=0;
+            aray.forEach(coisa =>{
+                contagem = contagem + coisa.numVezes;});
+
+                aray.forEach(coisa =>{
+                    coisa.estatistica = (100 * coisa.numVezes/contagem).toFixed(2);
+                });
+
+        //isso aq põe em ordem de mais frequente até menos frequente
+            aray.sort( (a,b)= b.numVezes - a.numVezes);
+            
+            for(let j=0; j<aray.length; j++)
+                aray[j].id=j+1;
+    }
     
     async function verificaCorretos(palavra){
 
-        const errados=[];
-        errados.push(await leitorDeJSON("erros.json"));
-
+        const errados= await leitorDeJSON("erros.json");
         const corretos=  await leitorDeJSON("validos.json");
 
-        if (!errados || !corretos) {
-            console.error('Simplemente não carregou os arquivos JSON família');
+        if (!corretos){
+            console.error('não consegui ler "validos.json');
+            return false;
+        }
+
+        if (!errados) {
+            console.error('não consegui ler "erros.json"');
             return false;
         }
 
         let alguemJaErrou=false;
-        let contagem=0;
-
         const correto = corretos.some(coisa => palavra === coisa);
 
         if (correto)
@@ -46,33 +69,25 @@
             //verifica se alguem já tentou aquela palavra errada
                 for (let i = 0; i < errados.length; i++) {
                     if (palavra === errados[i].palavra) {
-                    alguemJaErrou = true;
-                    errados[i].numVezes++;
-                    break;
+                        alguemJaErrou = true;
+                        errados[i].numVezes++;
+                        break;
         }
     }
                 if(!alguemJaErrou){                  //se ninguém nunca cometeu esse erro, ele é guardado como um novo objeto
                     const idNovo= errados.length;
                     const novoErro= {
-                        "id": idNovo, 
+                        "id": idNovo+1, 
                         "palavra": palavra, 
                         "numVezes": 1, 
-                        "estatística":0}
+                        "estatistica":0}
                     errados.push(novoErro);
             }
-
-            //isso aqui atualiza a estatistica de erro de cada objeto no array errados
-                errados.forEach(coisa =>{
-                    contagem = contagem + coisa.numVezes;});
-
-                errados.forEach(coisa =>{
-                    coisa.estatística = (100 * coisa.numVezes/contagem).toFixed(2);
-                });
-        
+            estatisticas(errados);
         }
 
         //reecreve o JSON
-            editorJSON("erros.json", errados);
+        await editorJSON("erros.json", errados);
         return false;    
 
     }
