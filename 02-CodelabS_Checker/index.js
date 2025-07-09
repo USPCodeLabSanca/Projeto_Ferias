1744cba789a559f7d927bc9e27d354547c42ed20
Arthur Santana <90651161+arthursantana-dev@users.noreply.github.com>
2025-07-09 00:55:45 -0300

    feature/checagem de nome existente em "nomes/validos"

--- 02-CodelabS_Checker/index.js
+++ 02-CodelabS_Checker/index.js
@@ -9,6 +9,7 @@ import NomeInvalido from "./models/NomeInvalido.js"
 const app = express()
 
 app.use(cors({ origin: "*" }))
+app.use(express.json())
 
 app.listen(3000, () => {
     sequelize.authenticate().then(() => {
@@ -21,17 +22,45 @@ app.listen(3000, () => {
 
 app.post("/nomes/validos", async (req, res) => {
 
+    // NomeValido.findAll({
+    //     attributes: [
+    //         "nome",
+    //         [sequelize.fn("COUNT", sequelize.col("nome")), "count_nome"]
+    //     ],
+    //     group: ["nome"]
+    // }).then(r => {
+    //     console.log(r);
+    //     res.send(r)
+    // }
+    // )
+
+    const nome = req.body.nome
+
     NomeValido.findAll({
-        attributes: [
-            "nome",
-            [sequelize.fn("COUNT", sequelize.col("nome")), "count_nome"]
-        ],
-        group: ["nome"]
+        where: {
+            nome: nome
+        }
     }).then(r => {
-        console.log(r);
-        res.send(r)
-    }
-    )
+        if (r.length > 0) {
+            res.status(409)
+            res.send({
+                msg: "Nome já existe."
+            })
+            return
+        }
+
+        NomeValido.create({ nome: nome }).then(r => {
+            res.status(201)
+            res.send(r)
+
+        })
+
+    })
+
+})
+
+app.post("/verificar", async (req, res) => {
+    const nome = req.body.nome
 
 })
 
