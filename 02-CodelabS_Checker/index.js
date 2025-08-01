dfe7eb9e87f6e6b03f3a03bbd33c47a2750d4c1d
João Vitor Simplicio <joaovitorvs0309@gmail.com>
2025-08-01 15:55:56 -0300

    Rotas de nomes aleatórios e de verificação
    Adicionei as rotas que gera nomes aleatórios e o que faz as verificações se o nome está certo ou não, e salva em um arquivo json.

--- 02-CodelabS_Checker/index.js
+++ 02-CodelabS_Checker/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const fs = require('fs');
+
 // Middleware pra aceitar JSON no corpo das requisições
 app.use(express.json());
 
@@ -13,4 +15,35 @@ app.get('/', (req, res) => {
 // Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+//rota para nomes aleatorios
+app.get('/nomes/aleatorio', (req, res) => {
+  const nomesErrados = JSON.parse(fs.readFileSync('erros.json'));
+
+  const nomeAleatorio = nomesErrados[Math.floor(Math.random() * nomesErrados.length)];
+
+  res.json({nome: nomeAleatorio});
+
+});
+
+//rota para  verificar se o nome é valido ou não
+app.post('/verificar', (req, res) => {
+
+  const nomeRecebido = req.body.nome;
+
+  const validos = JSON.parse(fs.readFileSync('nomesValidos.json'));
+
+  if(validos.includes(nomeRecebido)){
+    res.json({valido: true});
+  }else{
+    const errosAntigos = JSON.parse(fs.readFileSync('erros.json'));
+
+    errosAntigos.push(nomeRecebido);
+
+    fs.writeFileSync('erros.json', JSON.stringify(errosAntigos, null, 2));
+
+    res.json({valido: false});
+  }
+
 });
