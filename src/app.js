889849a9cc4833290f338859915af749350acef8
Allan Silva <allanvss15@gmail.com>
2025-08-02 00:05:30 -0300

    Removendo comentários

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ const nomeRoutes = require('./route/nomes');
 
 app.use(express.json());
 
-// Rotas
 app.use('/', nomeRoutes);
 
-module.exports = app; // Exporta o app para o server usar
+module.exports = app;
--- src/route/nomes.js
+++ src/route/nomes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const nomeController = require('../controllers/nomesController')
 
-// exemplo de rota
 router.get('/nomes/aleatorio', nomeController.nomesAleatorios);
 router.post('/verificar', nomeController.verificar);
 router.post('/nomes/validos', nomeController.nomesValidos);
