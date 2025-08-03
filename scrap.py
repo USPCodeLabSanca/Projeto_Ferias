8b36c1784fe6150e478ca98d02ab39a3938db18e
Pedro Henrique de Sousa Prestes <pedrohsprestes@gmail.com>
2025-08-03 14:05:55 -0300

    feat: trocando dic por lista

--- scrap.py
+++ scrap.py
@@ -6,27 +6,27 @@ response = requests.get('https://www.puspsc.usp.br/cardapio/.')
 menu_only = SoupStrainer(class_='row-striping row-hover')
 soup = BeautifulSoup(response.content, 'html.parser', parse_only=menu_only)
 
-# Printa o cardápio e salva em um dicionário
-cardapio_dict = {}
-for cardapio_dia in soup.find_all(lambda tag: tag.name=='tr' and tag.text.strip() != ''):
-    semana = cardapio_dia.find('strong').text
-    print('----> ' + semana)
+# Printa o cardápio e salva em uma lista
+cardapio = []
+for cardapio_soup in soup.find_all(lambda tag: tag.name=='tr' and tag.text.strip() != ''):
+    dia = cardapio_soup.find('strong').text
+    print('----> ' + dia)
 
-    almoco = cardapio_dia.find(class_='column-2').text
+    almoco = cardapio_soup.find(class_='column-2').text
     print('\n----------Almoço--------')
     print(almoco if almoco else "INDISPONÍVEL")
 
-    jantar = cardapio_dia.find(class_='column-3').text
+    jantar = cardapio_soup.find(class_='column-3').text
     print('\n----------Jantar--------')
     print(jantar if jantar else "INDISPONÍVEL")
 
     print('\n')
 
-    cardapio_dict[semana] = (almoco, jantar)
+    cardapio.append((dia, almoco, jantar))
 
 # Procura as comidas favoritas no cardápio
 favoritas = ['lasanha', 'gelatina', 'estrogonofe', 'frango empanado']
-for dia, (almoco, jantar) in cardapio_dict.items():
+for (dia, almoco, jantar) in cardapio:
     for comida in favoritas:
         if comida in almoco.lower():
             print(f"\n----> {dia}: Encontrado '{comida}' no almoço!")
