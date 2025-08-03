d5039e97135b836a8672ae2ea90121e53ec4c063
luis9queiroz <luis9queiroz@gmail.com>
2025-08-03 08:25:44 -0300

    Limpando o código

--- 03-Bandejao_Quality/TBot.py
+++ 03-Bandejao_Quality/TBot.py
@@ -1,20 +1,20 @@
 import telebot
-from scrapper import criaMsg
+from scrapper import criaMsg, week
 
 myID= '8008442499'
 myToken= '8366697118:AAFp1ZCUYe3MCyyXIXcZarpjsX0arwORO9g'
 bot = telebot.TeleBot(myToken)
-comidas= "\n".join(criaMsg())
+comidas= "\n".join(criaMsg(week))
 
 @bot.message_handler(commands=['start']) 
 def start(msg: telebot.types.Message):
-    bot.reply_to(msg, 'Olá mundo!')
+    bot.reply_to(msg, 'Olá mundo! Para verificar que dias tem comida boa no bandeco digite o comando "/Bandeco"')
 
 @bot.message_handler(commands=['Bandeco']) 
-def start(msg: telebot.types.Message):
+def Bandeco(msg: telebot.types.Message):
     bot.reply_to(msg, comidas)
 
-if len(comidas):
-    bot.send_message(myID, comidas)
+#if len(comidas):
+    #bot.send_message(myID, comidas)
 
 bot.infinity_polling()
--- 03-Bandejao_Quality/scrapper.py
+++ 03-Bandejao_Quality/scrapper.py
@@ -3,16 +3,11 @@ from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 
 #Comidas que você deseja buscar se tá tendo no bandeco
-comidas=['Frango', 'Batata', 'Lasanha', 'Pudim', 'Estrogonofe', 'Ovo Frito', 'Pé de Moça', 'Brigadeirão' ]
+comidas=['Lasanha de PVT', 'Ovo Frito', 'Pé de Moça', 'Brigadeirão' ]
+week=('Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado')
 
-#Essa função recebe a string do html do site e tira todas as coisas desnecessárias pra fazer a leitura, tipo </br> e tals
-#Ela também quebra em duas strings, uma com as comidas do almoço e uma com as comidas do jantar
-def cleanup(abc: str, num: int, dia: str):
-    #Lista de coisas que deve-se remover
-    trash=['<br/>', str(dia), '</strong>', f'<tr class="row-{num}">', '<td class="column-1">', '</td>', '<tr>', '</tr>', '<strong>', '<td class="column-2">']
-    for t in trash:
-        abc=abc.replace(t, '') #apagando as chaves de HTML
-    abc=abc.replace('\n', ' ') #deixando um pouco mais legível caso for necessário desbugar
+#Essa função recebe a string do html do site e quebra em duas strings, uma com as comidas do almoço e uma com as comidas do jantar
+def cleanup(abc: str):
     end=abc.find('<td class="column-3">') #aq é onde acabam as comidas do almoço e começam a do jantar
     menu= list()
     menu.append(abc[:end]) #almoço
@@ -20,27 +15,23 @@ def cleanup(abc: str, num: int, dia: str):
     return menu
 
 #Essa função já coloca a comida (que foi separada pela cleanup() em almoço e jantar) de cada dia dentro de um dicionário
-def daysOfTheWeek(sopinha):
+def daysOfTheWeek(sopinha, sem:tuple):
     semana=dict()
-    semana['Segunda']=  cleanup(str(sopinha.find(class_="row-3")), 3, 'Segunda-feira')
-    semana['Terça']=  cleanup(str(sopinha.find(class_="row-5")), 5, 'Terça-feira')
-    semana['Quarta']=  cleanup(str(sopinha.find(class_="row-7")), 7, 'Quarta-feira')
-    semana['Quinta']=  cleanup(str(sopinha.find(class_="row-9")), 9, 'Quinta-feira')
-    semana['Sexta']=  cleanup(str(sopinha.find(class_="row-11")), 11, 'Sexta-feira')
-    semana['Sábado']=  cleanup(str(sopinha.find(class_="row-13")), 13, 'Sábado')
-
+    j=0
+    for i in range (3, 14, 2):
+        classe= "row-" + str(i)
+        semana[sem[j]]= cleanup(str(sopinha.find(class_=classe)))
+        j+=1
     return semana
-
+ 
 #Essa função é só pra pegar as datas em número, tipo 02/08/2025
-def daysOfTheMonth(sopinha):
+def daysOfTheMonth(sopinha, tp: tuple):
     dates=dict()
-    week=['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
     ph= str(sopinha.find(class_="row-1"))
     day1=datetime(int(ph[40:42]), int(ph[43:45]), int(ph[47:51]))
-
     dates['Segunda']= day1
     for i in range (1,6):
-        dates[week[i]]=day1+timedelta(days=i)
+        dates[tp[i]]=day1+timedelta(days=i)
 
     return dates
 
@@ -66,18 +57,17 @@ def searchComidaBoa(dic:dict, comidaBoa:list):
     return quaisDias
 
 #Essa função envia as mensagens das comidas boas que tem cada dia, tanto pro console quanto cria uma lista q vai ser enviada para o telegram
-def criaMsg():
+def criaMsg(tp: tuple):
     response = requests.get('https://www.puspsc.usp.br/cardapio/')
     soup = BeautifulSoup(response.content, 'html.parser')
-    comidaBoa= searchComidaBoa(daysOfTheWeek(soup), comidas)
-    dias= daysOfTheMonth(soup)
+    comidaBoa= searchComidaBoa(daysOfTheWeek(soup, week), comidas)
+    diasM=daysOfTheMonth(soup, week)
     exportando= list()
     for c in comidaBoa:
         diazinho=c['dia']
-        if diazinho in dias.keys():
-            msg= (f'Eba! No dia {dias[diazinho].strftime("%d/%m/%Y")} ({diazinho}) vai ter {c["comida"]} no {c["refeição"]}!!!')
+        if diazinho in tp:
+            msg= (f'Eba! No dia {diasM[diazinho].strftime("%d/%m/%Y")} ({diazinho}) vai ter {c["comida"]} no {c["refeição"]}!!!')
             print(msg)
             exportando.append(msg[:])
-
     return exportando
 
